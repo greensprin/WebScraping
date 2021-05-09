3fcbd6f556a4ec7df833d891ecb8f76d54cb9725
mmkmss <mngjxd03170317@gmail.com>
2021-05-10 00:50:34 +0900

    template update

--- template.py
+++ template.py
@@ -12,6 +12,14 @@ if __name__ == "__main__":
   # driver設定
   driver = scr_util.SetDriver()
 
+  # url設定
+  target_url = ""
+  print("access url:{}".format(target_url))
+
+  # webアクセス
+  driver.get(target_url)
+  scr_util.TimeWait()
+
   # ===== 処理開始 =====
 
 
